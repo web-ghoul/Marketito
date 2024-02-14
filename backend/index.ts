7d04e457ba1aec4efb4b7e687e55b9ac9312fbd9
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2024-02-15 00:56:32 +0200

    Backend Init

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log(1)
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Define a route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Marketito Server');
+});
+
+// Start the server
+app.listen(5000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
